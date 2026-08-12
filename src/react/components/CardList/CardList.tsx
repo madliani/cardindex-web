deac6951ee305e0bbe633ba5530b8cf6e8328859
madliani <unixsocket@hotmail.com>
2026-08-12 13:12:55 +0400

    fix: fix CardList component

--- src/react/components/CardList/CardList.tsx
+++ src/react/components/CardList/CardList.tsx
@@ -1,10 +1,19 @@
 import type { CardListProps } from "@react/components/CardList/CardList.type"
 
+import { Card as HCard, Typography } from "@heroui/react"
 import { Card } from "@react/components/Card/Card"
 import "@react/components/CardList/CardList.style.scss"
 
-const CardList = ({ cardList }: CardListProps) => (
+const CardList = ({ cardList, title }: CardListProps) => (
     <section className="card-list">
+        {title && (
+            <HCard>
+                <HCard.Header>
+                    <Typography.Heading level={2}>{title}</Typography.Heading>
+                </HCard.Header>
+            </HCard>
+        )}
+
         {cardList.map(card => (
             <Card desc={card.desc} key={card.id} title={card.title} />
         ))}
--- src/react/components/CardList/CardList.type.ts
+++ src/react/components/CardList/CardList.type.ts
@@ -2,4 +2,5 @@ import type { Card } from "@api/cards/cards.type"
 
 export interface CardListProps {
     cardList: Card[]
+    title?: string
 }
